fb58d1b1917b21f7607abac316f8ec566e61e4d8
Arman Ali <armnsrn173200@gmail.com>
2024-01-25 18:39:17 +0530

    update3 commit

--- Webistic/frontend/src/App.jsx
+++ Webistic/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header/Header.jsx'
 import Home from './components/Home/Home.jsx'
 import Enquiry from './components/Enquiry/Enquiry.jsx'
+import A from './components/f1/A.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Home/>
       <Enquiry />
+      <A />
     </div>
   )
 }

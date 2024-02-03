4bb24cdc3f7c298d97cf8e00fb6db5d91387dd67
Arman Ali <armnsrn173200@gmail.com>
2024-02-03 22:33:06 +0530

    upadated commit

---
+++ Webistic/frontend/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+
+import Page2 from './Page2';
+import Page3 from './Page3';
+import Page4 from './Page4';
+import Page5 from './Page5';
+import Header from './Header';
+import Footer from './Footer';
+import Page6 from './Page6';
+//import Header
+import Heade from './Header/Heade.jsx';
+import  Home from './Home/Home.jsx';
+import Enquiry from './Enquiry/Enquiry.jsx';
+import A from './f1/A.jsx';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div id='root'>
+      <Heade />
+      <Home />
+      <Enquiry />
+      <Page2/>
+      <Page3 />
+      <Footer/>
+      <Page4/>
+      <Header/>
+      <Page5/>
+      <Page6/>
+      <A/>
+    </div>
+  )
+}
+
+export default App;
+
